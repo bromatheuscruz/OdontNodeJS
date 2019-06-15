4f949206e5ee60e7ae5723a51ad1c06b05b6322d
Matheus Cruz <bromatheuscruz@gmail.com>
2019-06-15 13:33:07 -0300

    Padronizando as respostas da api

--- src/controllers/doctor-controller.js
+++ src/controllers/doctor-controller.js
@@ -1,33 +1,33 @@
 const repository = require("../repositories/doctor-repository");
 
 exports.create = async (req, res, next) => {
-    const { doctor } = req.body;
+  const { doctor } = req.body;
 
-    try {
-        await repository.create(doctor);
-        res.status(201).send();
-    } catch (err) {
-        res.status(401).send({
-            success: false,
-            message: "Doctor not created",
-            error: err
-        });
-    }
-}
+  try {
+    await repository.create(doctor);
+    res.status(201).send();
+  } catch (err) {
+    res.status(401).send({
+      success: false,
+      message: "Doctor not created",
+      error: err
+    });
+  }
+};
 
 exports.getAll = async (req, res, next) => {
-    try {
-        const allDoctors = await repository.getAll();
-        res.status(200).send({
-            success: true,
-            message: "Success",
-            doctors: allDoctors
-        });
-    } catch (err) {
-        res.status(500).send({
-            success: false,
-            message: "Error while getting all doctors",
-            error: err
-        });
-    }
+  try {
+    const allDoctors = await repository.getAll();
+    res.status(200).send({
+      success: true,
+      message: "Success",
+      data: allDoctors
+    });
+  } catch (err) {
+    res.status(500).send({
+      success: false,
+      message: "Error while getting all doctors",
+      error: err
+    });
+  }
 };
--- src/controllers/pacient-controller.js
+++ src/controllers/pacient-controller.js
@@ -1,35 +1,33 @@
 const repository = require("../repositories/pacient-repository");
 
 exports.create = async (req, res, next) => {
-    const { pacient } = req.body;
+  const { pacient } = req.body;
 
-    try {
-        await repository.create(pacient);
-        res.status(201).send();
-    } catch (err) {
-        res
-            .status(201)
-            .send({
-                success: false,
-                message: "Pacient not created",
-                error: err
-            });
-    }
-}
+  try {
+    await repository.create(pacient);
+    res.status(201).send();
+  } catch (err) {
+    res.status(201).send({
+      success: false,
+      message: "Pacient not created",
+      error: err
+    });
+  }
+};
 
 exports.getAll = async (req, res, next) => {
-    try {
-        const allPacients = await repository.getAll();
-        res.status(200).send({
-            success: true,
-            message: "Success",
-            pacients: allPacients
-        });
-    } catch (err) {
-        res.status(500).send({
-            success: false,
-            message: "Error while getting all pacients",
-            error: err
-        });
-    }
+  try {
+    const allPacients = await repository.getAll();
+    res.status(200).send({
+      success: true,
+      message: "Success",
+      data: allPacients
+    });
+  } catch (err) {
+    res.status(500).send({
+      success: false,
+      message: "Error while getting all pacients",
+      error: err
+    });
+  }
 };
