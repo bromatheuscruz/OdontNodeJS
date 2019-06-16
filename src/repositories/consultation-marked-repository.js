0ddaadc363b79b360eb8587c39a8be9aebeb6444
Matheus Cruz <bromatheuscruz@gmail.com>
2019-06-16 00:54:29 -0300

    Buscando pacient e doctor ao consultar uma ConsultationMarked por id

--- src/repositories/consultation-marked-repository.js
+++ src/repositories/consultation-marked-repository.js
@@ -12,8 +12,29 @@ exports.getAll = async () => {
 };
 
 exports.getByPacientId = async id => {
-  const consultationMarkeds = await ConsultationMarked.find({
-    pacient: id
-  });
+  const consultationMarkeds = await ConsultationMarked.aggregate([
+    {
+      $match: {
+        pacient: new mongoose.Types.ObjectId(id)
+      }
+    },
+    {
+      $lookup: {
+        from: "doctors",
+        localField: "doctor",
+        foreignField: "_id",
+        as: "doctor"
+      }
+    },
+    {
+      $lookup: {
+        from: "pacients",
+        localField: "pacient",
+        foreignField: "_id",
+        as: "pacient"
+      }
+    }
+  ]);
+
   return consultationMarkeds;
 };
