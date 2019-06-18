696527391a654612ec9621855e6d89507eb11dba
Matheus Cruz <bromatheuscruz@gmail.com>
2019-06-17 21:09:17 -0300

    Remodelando schema pacient

--- src/db/schemas/pacient-schema.js
+++ src/db/schemas/pacient-schema.js
@@ -6,9 +6,55 @@ const Pacient = new Schema({
         type: String,
         required: true
     },
-    document: {
+    indication: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Pacient'
+    },
+    momName: {
+        type: String,
+    },
+    address: {
+        type: {
+            district: {
+                type: String,
+                required: true
+            },
+            city: {
+                type: String,
+                required: true
+            },
+            state: {
+                type: String,
+                required: true
+            },
+            CEP: {
+                type: String,
+                required: true
+            }
+        }
+    },
+    rg: {
         type: String,
         required: true
+    },
+    cpf: {
+        type: String,
+        required: true
+    },
+    phones: [
+        {
+            type: String
+        }
+    ],
+    email: {
+        type: String,
+        required: true
+    },
+    birthday: {
+        type: Date
+    },
+    observation: {
+        type: String
     }
 });
 
