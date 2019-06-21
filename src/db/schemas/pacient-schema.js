939df087aea1daa9d62f5913dfd27f826ee9798a
Matheus Cruz <bromatheuscruz@gmail.com>
2019-06-21 19:20:03 -0300

    Iniciando testes de Paciente

--- src/db/schemas/pacient-schema.js
+++ src/db/schemas/pacient-schema.js
@@ -2,60 +2,66 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Pacient = new Schema({
-    name: {
+  name: {
+    type: String,
+    required: true
+  },
+  indication: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Pacient"
+  },
+  momName: {
+    type: String
+  },
+  address: {
+    type: {
+      district: {
         type: String,
         required: true
-    },
-    indication: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Pacient'
-    },
-    momName: {
-        type: String,
-    },
-    address: {
-        type: {
-            district: {
-                type: String,
-                required: true
-            },
-            city: {
-                type: String,
-                required: true
-            },
-            state: {
-                type: String,
-                required: true
-            },
-            CEP: {
-                type: String,
-                required: true
-            }
-        }
-    },
-    rg: {
+      },
+      city: {
         type: String,
         required: true
-    },
-    cpf: {
+      },
+      state: {
         type: String,
         required: true
-    },
-    phones: [
-        {
-            type: String
-        }
-    ],
-    email: {
+      },
+      CEP: {
         type: String,
         required: true
-    },
-    birthday: {
-        type: Date
-    },
-    observation: {
+      },
+      number: {
         type: String
+      },
+      complement: {
+        type: String
+      }
     }
+  },
+  rg: {
+    type: String,
+    required: true
+  },
+  cpf: {
+    type: String,
+    required: true
+  },
+  phones: [
+    {
+      type: String
+    }
+  ],
+  email: {
+    type: String,
+    required: true
+  },
+  birthday: {
+    type: Date
+  },
+  observation: {
+    type: String
+  }
 });
 
 module.exports = mongoose.model("Pacient", Pacient);
